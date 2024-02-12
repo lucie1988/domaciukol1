52e1ec896c045844fdab529248b83f059b1fee57
Lucie Panagopulos <luciepanagopulos@gmail.com>
2024-02-12 23:24:32 +0100

    Do složky script vyplnen prvni domaci ukol

---
+++ script.js
@@ -0,0 +1,5 @@
+let fahrenheit = prompt ("Uveďte teplotu ve stupních Fahrenheita")
+let celsia = (fahrenheit - 32) * 5/9
+
+document.body.innerHTML += "Ekvivalent ve stupních celsia je " + (fahrenheit - 32) * 5/9 
+
